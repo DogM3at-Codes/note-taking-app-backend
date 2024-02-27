76fca62d0529977ab38bbf68bfb79a03a2d8b351
Joel Carlo M. Menor <menorjc@live.com>
2024-02-28 01:29:00 +0800

    Validation for post and put. Error handling

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import notebookRoutes from './routes/routes'
 
 const app = express()
@@ -6,6 +6,11 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use('/api/v1/notes', notebookRoutes)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res.status(500).send('Unhandled Error');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
